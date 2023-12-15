b150c32a6f03b8e4f3c15bdd769ce9324b0cf06d
Pablo Tomas Borda <brainboost@brainboost.ie>
2023-12-15 16:43:29 +0000

    last version

--- .next/server/middleware-react-loadable-manifest.js
+++ .next/server/middleware-react-loadable-manifest.js
@@ -1,1 +1,1 @@
-self.__REACT_LOADABLE_MANIFEST="{}"
+self.__REACT_LOADABLE_MANIFEST="{\"index.js -> ../components/ThreeCanvas\":{\"id\":\"index.js -> ../components/ThreeCanvas\",\"files\":[\"static/chunks/components_ThreeCanvas_js.js\"]}}"
