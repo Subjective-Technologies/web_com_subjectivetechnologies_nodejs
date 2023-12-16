296a91ae1913cce66d8465ea3c240da63956cdb3
Pablo Tomas Borda <brainboost@brainboost.ie>
2023-12-16 16:46:34 +0000

    lasr

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a190e005824b73a0"; }
+/******/ 		__webpack_require__.h = function() { return "c9d92c1cb5795a24"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/GoldenThinkerAnimation.js
+++ components/GoldenThinkerAnimation.js
@@ -8,25 +8,36 @@ import Scene from './Scene';
 const GoldenThinkerAnimation = () => {
   useEffect(() => {
     const image360Path = 'images/360/back.jpg'; // Update with the actual path
-    const objPath = '3d/anim_goldenthinker.obj'; // Update with the actual path
-
-
+   // const objPath = '3d/anim_goldenthinker.obj'; // Update with the actual path
+    
+    const objPath = '3d/anim_menu.obj';
+    // Create a gold material
     const goldMaterial = new THREE.MeshPhysicalMaterial({
-      color: 0xFFD700, // Gold color
-      metalness: 1.0, // Full metalness for a gold-like effect
-      roughness: 0.4, // Adjust for shininess, lower values are shinier
-      // Refraction properties can be added here if needed
-  });
-  
-
-
-
+      color: 0xD4AF37, // Gold color
+      metalness: 1, // Full metalness makes it look more like gold
+      roughness: 0.3, // Adjust to make it shinier or more matte
+      reflectivity: 1, // Full reflectivity for a metallic look
+    });
     const myScene = new Scene(image360Path,objPath, goldMaterial);
-    myScene.init();
+    
+
+   
+
+     myScene.addElement(objPath, {
+       material: goldMaterial,
+       position: { x: 0, y: -2, z: 0 },
+       rotation: { x: 0, y: 0, z: 0 },
+     });
+
+
+
+
+
+
 
     return () => {
       // Clean up when the component unmounts
-      myScene.dispose();
+      //myScene.dispose();
     };
   }, []);
 
