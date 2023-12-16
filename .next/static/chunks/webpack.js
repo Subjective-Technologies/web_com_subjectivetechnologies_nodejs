222ecb8d7e3eff91dbb3fbc6f4cd9bbecd532d7b
Pablo Tomas Borda <brainboost@brainboost.ie>
2023-12-16 01:34:21 +0000

    wors

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -153,7 +153,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b4f4ec2b41811b7"; }
+/******/ 		__webpack_require__.h = function() { return "9b01b1eaeadd5791"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/GoldenThinkerAnimation.js
+++ components/GoldenThinkerAnimation.js
@@ -10,7 +10,18 @@ const GoldenThinkerAnimation = () => {
     const image360Path = 'images/360/back.jpg'; // Update with the actual path
     const objPath = '3d/anim_goldenthinker.obj'; // Update with the actual path
 
-    const myScene = new Scene(image360Path, objPath);
+
+    const goldMaterial = new THREE.MeshPhysicalMaterial({
+      color: 0xFFD700, // Gold color
+      metalness: 1.0, // Full metalness for a gold-like effect
+      roughness: 0.4, // Adjust for shininess, lower values are shinier
+      // Refraction properties can be added here if needed
+  });
+  
+
+
+
+    const myScene = new Scene(image360Path,objPath, goldMaterial);
     myScene.init();
 
     return () => {
